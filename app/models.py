d88f6be2580fd7ed83482223aa089f6813143381
devseme <ian.ochenge@student.moringaschool.com>
2021-11-12 13:35:40 +0300

    started on the main files of the root folders

---
+++ app/models.py
@@ -0,0 +1,43 @@
+from . import db,login_manager
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from datetime import datetime
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+   # user table
+class User(UserMixin, db.Model):  
+    _tablename_ = 'user'
+    # posts = db.relationship('Post', backref='author', lazy='dynamic')
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True, index=True)
+    password_hash = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+     
+
+     # generate password hash
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    # check password
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    # login manager
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    def _repr_(self):
+        return f'User {self.username}'
+ 
