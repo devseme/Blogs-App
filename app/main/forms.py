b2a608a3e77d5c447e73fd643b4ee103ef9e1ca8
devseme <ian.ochenge@student.moringaschool.com>
2021-11-13 18:44:05 +0300

    working on profile and blogs

--- app/main/forms.py
+++ app/main/forms.py
@@ -13,3 +13,14 @@ class BlogForm(FlaskForm):
     content = TextAreaField('Body', validators=[Required()])
     created_by= StringField('Blog created_by',validators=[Required()])
     submit = SubmitField('Submit')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell Us About Yourself...',validators = [Required()])
+    submit = SubmitField('Submit')
+
+class UpdateProfileForm(FlaskForm):
+    name = StringField('Name', validators=[Required(), Length(1, 64)])
+    username = StringField('Username', validators=[Required(), Length(1, 64)])
+    email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
+    bio = TextAreaField('About...', validators=[Required(), Length(1, 100)])
+    submit = SubmitField('Submit')    
